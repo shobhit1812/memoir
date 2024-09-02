6ebeb8a78add4b495ec06fc1022b313db55dc36e
Shobhit Nautiyal <shobhit.nautiyal99@gmail.com>
2024-09-02 18:50:14 +0530

    Add functionality to edit a blog

--- server/src/controllers/blog.controller.js
+++ server/src/controllers/blog.controller.js
@@ -4,6 +4,14 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { v2 as cloudinary } from "cloudinary";
+
+const extractPublicId = (url) => {
+  const parts = url.split("/");
+  const fileNameWithExtension = parts[parts.length - 1]; // e.g., "sample.jpg"
+  const [publicId] = fileNameWithExtension.split("."); // Split by "." to remove file extension
+  return publicId;
+};
 
 const createBlog = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
@@ -41,6 +49,57 @@ const createBlog = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, blog, "Blog created successfully"));
 });
 
+const editBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.params;
+  const { title, description } = req.body;
+
+  const blog = await Blog.findById(blogId);
+
+  if (!blog) {
+    throw new ApiError(404, "Blog not found");
+  }
+
+  if (blog.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to edit this blog");
+  }
+
+  let coverImage;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    const coverImageLocalPath = req.files.coverImage[0].path;
+    const path = "memoir/coverImage";
+
+    const folder_name = "coverImage";
+    coverImage = await uploadOnCloudinary(coverImageLocalPath, folder_name);
+
+    if (!coverImage) {
+      throw new ApiError(400, "Failed to upload cover image to Cloudinary");
+    }
+
+    // Removing the old cover image from Cloudinary
+    if (blog.coverImage) {
+      const publicId = extractPublicId(blog.coverImage); // Function to extract the public ID
+      await cloudinary.uploader.destroy(`${path}/${publicId}`);
+    }
+
+    // Update the blog's cover image URL
+    blog.coverImage = coverImage.secure_url;
+  }
+
+  blog.title = title || blog.title;
+  blog.description = description || blog.description;
+
+  await blog.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, blog, "Blog updated successfully"));
+});
+
 const getAllBlogs = asyncHandler(async (_, res) => {
   try {
     const blogs = await Blog.find({})
@@ -64,4 +123,4 @@ const getAllBlogs = asyncHandler(async (_, res) => {
   }
 });
 
-export { createBlog, getAllBlogs };
+export { createBlog, editBlog, getAllBlogs };
--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createBlog, getAllBlogs } from "../controllers/blog.controller.js";
+import {
+  createBlog,
+  editBlog,
+  getAllBlogs,
+} from "../controllers/blog.controller.js";
 
 const router = Router();
 
@@ -16,6 +20,16 @@ router.route("/create-blog").post(
   createBlog
 );
 
+router.route("/edit-blog/:blogId").post(
+  verifyJWT,
+  upload.fields([
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  editBlog
+);
 router.route("/get-all-blogs").get(verifyJWT, getAllBlogs);
 
 export default router;
