2eea07b6b248e1993a80963ffb4e2b06a6fb1dd8
Shobhit Nautiyal <88445550+shobhit1812@users.noreply.github.com>
2024-09-13 11:53:18 +0530

    Update Footer component with author information and copyright notice

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,20 @@
 const Footer = () => {
   return (
     <footer className="p-4 text-center text-md">
-      <p>Footer content goes here</p>
+      <div className="text-white text-base p-5 pb-10 flex justify-center">
+        <h1>
+          <a
+            href="https://github.com/Shobhit1812"
+            target="_blank"
+            className="hover:text-[#414141]"
+          >
+            Shobhit Nautiyal
+          </a>{" "}
+          | No © copy right issues.
+          <span className="italic"> Feel Free To Copy. </span>
+          If you need any help, ping me!
+        </h1>
+      </div>
     </footer>
   );
 };
